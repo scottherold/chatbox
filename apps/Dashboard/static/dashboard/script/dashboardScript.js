b8b5b7f7ad47ef3e0e798bf4762d3622d56b6adc
Aaron Dasani <maad@Aarons-iMac.local>
2018-11-04 02:39:15 -0800

    friend feature done

--- apps/Dashboard/static/dashboard/script/dashboardScript.js
+++ apps/Dashboard/static/dashboard/script/dashboardScript.js
@@ -58,4 +58,25 @@ $(document).ready(function () {
         $(".editPasswordSection").hide();
 
     })
-})
+
+    // unfriend effects
+    
+    $(".friendBTN").hover(
+        function(){
+            $(".fa-user-friends+span").text("Unfriend");
+            $(this).removeClass( "btn-primary" );
+            $(this).toggleClass( "btn-warning" );
+        }, 
+        function(){
+            $(".fa-user-friends+span").text("Friend");
+            $(this).removeClass( "btn-warning" );
+            $(this).toggleClass( "btn-primary" );
+
+        } 
+        
+        
+        
+    );
+    
+    
+});
--- apps/friends/urls.py
+++ apps/friends/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 urlpatterns = [
     # url(r'^$', views.index, name="index"),
-    url(r'^create/$', views.create, name="create"),
-    url(r'^(?P<id>\d+)/update/$', views.update, name="update"),
-    url(r'^(?P<id>\d+)/destroy/$', views.destroy, name="destroy"),
+    url(r'^create/(?P<user_id>\d+)$', views.create, name="create"),
+     url(r'^destroy/(?P<user_id>\d+)/$', views.delete, name="delete"),
+
+
+
     url(r'^new/$', views.new, name="new"),
     url(r'^(?P<id>\d+)/edit/$', views.edit, name="edit"),
     url(r'^(?P<id>\d+)/$', views.show, name="show"),
